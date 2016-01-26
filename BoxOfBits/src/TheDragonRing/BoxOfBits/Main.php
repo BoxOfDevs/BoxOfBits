4ff0da5459e2debc205d23cb0079e32f3cc22886
TheDragonRing <tom.vines@rocketmail.com>
2016-01-26 15:36:20 +1100

    NEED TO ADD /sendpopup /sendbroadcast

--- BoxOfBits/src/TheDragonRing/BoxOfBits/Main.php
+++ BoxOfBits/src/TheDragonRing/BoxOfBits/Main.php
@@ -263,13 +263,13 @@ class Main extends PluginBase implements Listener{
 			$player = $this->getServer()->getPlayer($sender->getName());
 			if($player->hasPermission("boxofbits.rules")){
 			if(!isset($args[0])){
-				$sender->sendMessage(Colour::BLACK. "---[".Colour::DARK_PURPLE."BoxOfBits v0.0.1".Colour::BLACK."]---");
+				$sender->sendMessage(Colour::BLACK. "---[".Colour::GOLD."BoxOfBits v0.0.1".Colour::BLACK."]---");
 				$sender->sendMessage(Colour::DARK_RED. "Usage: " .Colour::WHITE."/rules 1|2".Colour::DARK_RED." Shows page 1 or 2 of server rules");
 				return true;
 			}else{
 				switch ($args[0]){
 					case "1":
-						$sender->sendMessage(Colour::BLACK. "---[".Colour::DARK_PURPLE."Server Rules Page 1".Colour::BLACK."]---");
+						$sender->sendMessage(Colour::BLACK. "---[".Colour::GOLD."Server Rules Page 1".Colour::BLACK."]---");
 						$sender->sendMessage(Colour::BLACK. "- " .Colour::WHITE($this->config->get("Rule1")));
 						$sender->sendMessage(Colour::BLACK. "- " .Colour::WHITE($this->config->get("Rule2")));
 						$sender->sendMessage(Colour::BLACK. "- " .Colour::WHITE($this->config->get("Rule3")));
@@ -278,7 +278,7 @@ class Main extends PluginBase implements Listener{
 						return true;
 							break;
 					case "2":
-						$sender->sendMessage(Colour::BLACK. "---[".Colour::DARK_PURPLE."Server Rules Page 2".Colour::BLACK."]---");
+						$sender->sendMessage(Colour::BLACK. "---[".Colour::GOLD"Server Rules Page 2".Colour::BLACK."]---");
 						$sender->sendMessage(Colour::BLACK. "- " .Colour::WHITE($this->config->get("Rule6")));
 						$sender->sendMessage(Colour::BLACK. "- " .Colour::WHITE($this->config->get("Rule7")));
 						$sender->sendMessage(Colour::BLACK. "- " .Colour::WHITE($this->config->get("Rule8")));
