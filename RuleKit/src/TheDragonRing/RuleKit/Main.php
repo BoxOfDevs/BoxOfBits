8ff270ce82f7686066996d47432c15a27e74a4f2
TheDragonRing <tom.vines@rocketmail.com>
2016-01-31 17:43:03 +1100

    fixed main.php

--- RuleKit/src/TheDragonRing/RuleKit/Main.php
+++ RuleKit/src/TheDragonRing/RuleKit/Main.php
@@ -66,20 +66,20 @@ class Main extends PluginBase implements Listener{
 				switch ($args[0]){
 					case "1":
 						$sender->sendMessage(Colour::BLACK. "---[".Colour::DARK_PURPLE."Server Rules Page 1".Colour::BLACK."]---");
-						$sender->sendMessage(Colour::BLACK. "- " .Colour::WHITE($this->config->get("Rule1")));
-						$sender->sendMessage(Colour::BLACK. "- " .Colour::WHITE($this->config->get("Rule2")));
-						$sender->sendMessage(Colour::BLACK. "- " .Colour::WHITE($this->config->get("Rule3")));
-						$sender->sendMessage(Colour::BLACK. "- " .Colour::WHITE($this->config->get("Rule4")));
-						$sender->sendMessage(Colour::BLACK. "- " .Colour::WHITE($this->config->get("Rule5")));
+						$sender->sendMessage(Colour::BLACK. "- " ($this->config->get("Rule1")));
+						$sender->sendMessage(Colour::BLACK. "- " ($this->config->get("Rule2")));
+						$sender->sendMessage(Colour::BLACK. "- " ($this->config->get("Rule3")));
+						$sender->sendMessage(Colour::BLACK. "- " ($this->config->get("Rule4")));
+						$sender->sendMessage(Colour::BLACK. "- " ($this->config->get("Rule5")));
 						return true;
 							break;
 					case "2":
 						$sender->sendMessage(Colour::BLACK. "---[".Colour::DARK_PURPLE."Server Rules Page 1".Colour::BLACK."]---");
-						$sender->sendMessage(Colour::BLACK. "- " .Colour::WHITE($this->config->get("Rule6")));
-						$sender->sendMessage(Colour::BLACK. "- " .Colour::WHITE($this->config->get("Rule7")));
-						$sender->sendMessage(Colour::BLACK. "- " .Colour::WHITE($this->config->get("Rule8")));
-						$sender->sendMessage(Colour::BLACK. "- " .Colour::WHITE($this->config->get("Rule9")));
-						$sender->sendMessage(Colour::BLACK. "- " .Colour::WHITE($this->config->get("Rule10")));
+						$sender->sendMessage(Colour::BLACK. "- " ($this->config->get("Rule6")));
+						$sender->sendMessage(Colour::BLACK. "- " ($this->config->get("Rule7")));
+						$sender->sendMessage(Colour::BLACK. "- " ($this->config->get("Rule8")));
+						$sender->sendMessage(Colour::BLACK. "- " ($this->config->get("Rule9")));
+						$sender->sendMessage(Colour::BLACK. "- " ($this->config->get("Rule10")));
 							return true;
 						}
 					}
