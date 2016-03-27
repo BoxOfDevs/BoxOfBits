5a1d132334850ac2c0d4a2fb832e88986dc92d70
TheDragonRing <tom.vines@rocketmail.com>
2016-03-27 21:44:15 +1100

    Create health.php

---
+++ src/TheDragonRing/BoxOfBits/Commands/health.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+  ____             ____   __ ____  _ _       
+ |  _ \           / __ \ / _|  _ \(_) |      
+ | |_) | _____  _| |  | | |_| |_) |_| |_ ___ 
+ |  _ < / _ \ \/ / |  | |  _|  _ <| | __/ __|
+ | |_) | (_) >  <| |__| | | | |_) | | |_\__ \
+ |____/ \___/_/\_\\____/|_| |____/|_|\__|___/
+ 
+ The growing plugin with so many features
+ 
+ */
+ 
+namespace TheDragonRing\BoxOfBits\Commands;
+ 
+use TheDragonRing\BoxOfBits\Loader;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\PluginCommand;
+use pocketmine\permission\Permission;
+use pocketmine\utils\Config;
+use pocketmine\Player;
+use pocketmine\Server;
+
+class health extends Loader{
+    
+    private $permMessage = "§4You do not have permission to run this command!";
+    private $consoleMsg = "§4This command can only be executed in-game!"; 
+    
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+        if(strolower($cmd->getName() == "health")){
+            if(!($sender instanceof Player)){
+				if(!isset($args[1])){
+					$sender->sendMessage("§4Usage: /health <amount> [playername] - [playername] required when run from console!");
+				}else{
+				    $player = $this->getServer()->getPlayer($args[1]);
+				    if($player instanceof Player){
+				        $player->setHealth($args[0]);
+				        $sender->sendMessage("§b$player's health has been set");
+				        $player->sendMessage("§bYour health has been set");
+				    }else{
+				        $sender->sendMessage("§4Player Not Found");
+				    }
+				}
+            }
+            if($sender instanceof Player){
+                if($sender->hasPermission("boxofbits.health")){
+                    if(!isset($args[0])){
+                        $sender->sendMessage("§4Usage: /health <amount> [playername]");
+                    }
+				    if(!isset($args[1])){
+                        $player->setHealth($args[0]);
+                        $sender->sendMessage("§bYour health has been set");
+                    }else{
+				        $player = $this->getServer()->getPlayer($args[1]);
+				        if($player instanceof Player){
+				            $player->setHealth($args[0]);
+				            $sender->sendMessage("§b$player's health has been set");
+				            $player->sendMessage("§bYour health has been set");
+				        }else{
+				            $sender->sendMessage("§4Player Not Found");
+				        }
+				    }
+				}else{
+                $sender->sendMessage("$this->permMessage");
+				}
+            }
+        }
+        return true;
+    }
+
+}
