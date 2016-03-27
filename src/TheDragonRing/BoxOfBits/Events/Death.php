b8323b0f3f4686cc94a517cac615f745e76baf2b
TheDragonRing <tom.vines@rocketmail.com>
2016-03-27 20:56:38 +1100

    Create Death.php

---
+++ src/TheDragonRing/BoxOfBits/Events/Death.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+  ____             ____   __ ____  _ _       
+ |  _ \           / __ \ / _|  _ \(_) |      
+ | |_) | _____  _| |  | | |_| |_) |_| |_ ___ 
+ |  _ < / _ \ \/ / |  | |  _|  _ <| | __/ __|
+ | |_) | (_) >  <| |__| | | | |_) | | |_\__ \
+ |____/ \___/_/\_\\____/|_| |____/|_|\__|___/
+ 
+ The growing plugin with so many features, an alternative to Essentials or EssentialsPE
+ 
+*/
+
+namespace TheDragonRing\BoxOfBits\Events;
+
+use TheDragonRing\BoxOfBits\Loader;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat as Colour;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\utils\Config;
+
+class Death extends Loader implements Listener{
+    
+    public function onDeath(PlayerDeathEvent $event){
+        $player = $event->getPlayer();
+        $name = $player->getName();
+        $line = "/n";
+        $popup = str_replace("{player}", $name, "{line}", $line, $this->config->get("DeathPopup"));
+        $this->getServer()->broadcastPopup($popup);
+        $message = str_replace("{player}", $name, "{line}", $line, $this->config->get("DeathMessage"));
+        $this->getServer()->broadcastMessage($message);
+        return true;
+    }
+
+}
